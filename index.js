aaba4f128d34c212d8d82b0462cfd6934eb16264
laboud <yespro2019@gmail.com>
2025-01-27 11:39:47 +0100

    ss

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 let now_playing = document.querySelector('.now-playing');
 let track_art = document.querySelector('.track-art');
 let track_name = document.querySelector('.track-name');
@@ -11,105 +12,70 @@ let seek_slider = document.querySelector('.seek_slider');
 let volume_slider = document.querySelector('.volume_slider');
 let curr_time = document.querySelector('.current-time');
 let total_duration = document.querySelector('.total-duration');
-let wave = document.getElementById('wave');
-let randomIcon = document.querySelector('.fa-random');
 let curr_track = document.createElement('audio');
 
 let track_index = 0;
 let isPlaying = false;
-let isRandom = false;
-let updateTimer;
 
 const music_list = [
     {
         img: '../img/mh.jpg',
         name: 'AL-Fatiha - الفاتحة',
         artist: 'محمد اللحيدان',
-        audio: '../audio/dd.m4a'
+        audio: 'https://media.sd.ma/assabile/recitations_7892537823/mp3/mohammed-al-lohaidan-001-al-fatiha-8575-3583.mp3'
     },
     {
-        img: '../img/mh.jpg',
-        name: 'AL-Fatiha - الفاتحة',
+        img: '../img/mohamed-al-haidan.png',
+        name: 'AL-Baqara - البقرة',
         artist: 'محمد اللحيدان',
-        audio: '../audio/Beautiful_recitation.m4a'
+        audio: 'https://media.sd.ma/assabile/recitations_7892537823/mp3/mohammed-al-lohaidan-002-al-baqara-8576-3797.mp3'
+    },
+    {
+        img: '../img/mohamed-al-haidan-2899.jpg',
+        name: 'AL-imran - العمران',
+        artist: 'محمد اللحيدان',
+        audio: 'https://media.sd.ma/assabile/recitations_7892537823/mp3/mohammed-al-lohaidan-003-aal-e-imran-8577-175.mp3'
     }
 ];
 
 function loadTrack(track_index) {
-    clearInterval(updateTimer);
-    reset();
-
     curr_track.src = music_list[track_index].audio;
     curr_track.load();
 
-    track_art.style.backgroundImage = `url(${music_list[track_index].img})`;
+    track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
     track_name.textContent = music_list[track_index].name;
     track_artist.textContent = music_list[track_index].artist;
-    now_playing.textContent = `Playing ${track_index + 1} of ${music_list.length}`;
-
-    updateTimer = setInterval(setUpdate, 1000);
-    curr_track.addEventListener('ended', nextTrack);
+    now_playing.textContent = "Playing " + (track_index + 1) + " of " + music_list.length;
 }
 
-function reset() {
-    curr_time.textContent = "00:00";
-    total_duration.textContent = "00:00";
-    seek_slider.value = 0;
+function playpauseTrack() {
+    if (!isPlaying) {
+        curr_track.play();
+        isPlaying = true;
+        playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+    } else {
+        curr_track.pause();
+        isPlaying = false;
+        playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+    }
 }
 
-function randomTrack() {
-    isRandom ? pauseRandom() : playRandom();
-}
-
-function playRandom() {
-    isRandom = true;
-    randomIcon.classList.add('randomActive');
-}
-
-function pauseRandom() {
-    isRandom = false;
-    randomIcon.classList.remove('randomActive');
-}
-
-function repeatTrack() {
+function nextTrack() {
+    track_index = (track_index + 1) % music_list.length;
     loadTrack(track_index);
     playTrack();
 }
 
-function playpauseTrack() {
-    isPlaying ? pauseTrack() : playTrack();
+function prevTrack() {
+    track_index = (track_index - 1 + music_list.length) % music_list.length;
+    loadTrack(track_index);
+    playTrack();
 }
 
 function playTrack() {
     curr_track.play();
     isPlaying = true;
     playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
-}
-
-function pauseTrack() {
-    curr_track.pause();
-    isPlaying = false;
-    playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
-}
-
-function nextTrack() {
-    if (track_index < music_list.length - 1) {
-        track_index += 1;
-    } else {
-        track_index = 0;
-    }
-    loadTrack(track_index);
-    playTrack();
-}
-
-function prevTrack() {
-    if (track_index > 0) {
-        track_index -= 1;
-    } else {
-        track_index = music_list.length - 1;
-    }
-    loadTrack(track_index);
-    playTrack();
 }
 
 function seekTo() {
@@ -121,20 +87,17 @@ function setVolume() {
     curr_track.volume = volume_slider.value / 100;
 }
 
-function setUpdate() {
-    let seekPosition = 0;
-    if (!isNaN(curr_track.duration)) {
-        seekPosition = curr_track.currentTime * (100 / curr_track.duration);
-        seek_slider.value = seekPosition;
+curr_track.addEventListener('timeupdate', function () {
+    let seekPosition = (curr_track.currentTime / curr_track.duration) * 100;
+    seek_slider.value = seekPosition;
 
-        let currentMinutes = Math.floor(curr_track.currentTime / 60);
-        let currentSeconds = Math.floor(curr_track.currentTime % 60);
-        let durationMinutes = Math.floor(curr_track.duration / 60);
-        let durationSeconds = Math.floor(curr_track.duration % 60);
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
 
-        curr_time.textContent = `${currentMinutes}:${currentSeconds}`;
-        total_duration.textContent = `${durationMinutes}:${durationSeconds}`;
-    }
-}
+    curr_time.textContent = `${currentMinutes}:${currentSeconds < 10 ? '0' : ''}${currentSeconds}`;
+    total_duration.textContent = `${durationMinutes}:${durationSeconds < 10 ? '0' : ''}${durationSeconds}`;
+});
 
 loadTrack(track_index);

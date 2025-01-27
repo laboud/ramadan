c84a7b6fe8892ddcb6940d52ad811c7e6e7c85f9
laboud <yespro2019@gmail.com>
2025-01-27 11:03:32 +0100

    l7amd leh

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let now_playing = document.querySelector('.now-playing');
 let track_art = document.querySelector('.track-art');
 let track_name = document.querySelector('.track-name');
@@ -23,154 +22,119 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'img/mh.jpg',
-        name : 'AL-fatiha-الفاتحة',
-        artist : 'محمد اللحيدان',
-        audio : 'audio/dd.m4a'
+        img: '../img/mh.jpg',
+        name: 'AL-Fatiha - الفاتحة',
+        artist: 'محمد اللحيدان',
+        audio: '../audio/dd.m4a'
     },
     {
-        
-        img : 'img/mh.jpg',
-        name : 'محمد اللحيدان',
-        artist : 'AL-fatiha-الفاتحة',
-        audio : 'audio/Beautiful recitation of surah Al-Fatiha with translation - Muhammad Al-Luhaidan.m4a'
-    },
-    {
-   
-        img : 'img/mh.jpg',
-        name : 'محمد اللحيدان',
-        artist : 'AL-fatiha-الفاتحة',
-        music : 'audio/Beautiful recitation of surah Al-Fatiha with translation - Muhammad Al-Luhaidan.m4a'
-    },
-    {
-        
-        img : 'img/mh.jpg',
-        name : 'محمد اللحيدان',
-        artist : 'AL-fatiha-الفاتحة',
-        music : 'audio/Beautiful recitation of surah Al-Fatiha with translation - Muhammad Al-Luhaidan.m4a'
+        img: '../img/mh.jpg',
+        name: 'AL-Fatiha - الفاتحة',
+        artist: 'محمد اللحيدان',
+        audio: '../audio/Beautiful_recitation.m4a'
     }
 ];
 
-loadTrack(track_index);
-
-function loadTrack(track_index){
+function loadTrack(track_index) {
     clearInterval(updateTimer);
     reset();
 
-    curr_track.src = music_list[track_index].music;
+    curr_track.src = music_list[track_index].audio;
     curr_track.load();
 
-    track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
+    track_art.style.backgroundImage = `url(${music_list[track_index].img})`;
     track_name.textContent = music_list[track_index].name;
     track_artist.textContent = music_list[track_index].artist;
-    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+    now_playing.textContent = `Playing ${track_index + 1} of ${music_list.length}`;
 
     updateTimer = setInterval(setUpdate, 1000);
-
     curr_track.addEventListener('ended', nextTrack);
-    random_bg_color();
 }
 
-function random_bg_color(){    
-
-    function populate(a){
-        for(let i=0; i<6; i++){
-            let x = Math.round(Math.random() * 14);
-            let y = hex[x];
-            a += y;
-        }
-        return a;
-    }
-    let Color1 = populate('#');
-    let Color2 = populate('#');
-    var angle = 'to right';
-
-    let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
-    document.body.style.background = gradient;
-}
-function reset(){
+function reset() {
     curr_time.textContent = "00:00";
     total_duration.textContent = "00:00";
     seek_slider.value = 0;
 }
-function randomTrack(){
+
+function randomTrack() {
     isRandom ? pauseRandom() : playRandom();
 }
-function playRandom(){
+
+function playRandom() {
     isRandom = true;
     randomIcon.classList.add('randomActive');
 }
-function pauseRandom(){
+
+function pauseRandom() {
     isRandom = false;
     randomIcon.classList.remove('randomActive');
 }
-function repeatTrack(){
-    let current_index = track_index;
-    loadTrack(current_index);
+
+function repeatTrack() {
+    loadTrack(track_index);
     playTrack();
 }
-function playpauseTrack(){
+
+function playpauseTrack() {
     isPlaying ? pauseTrack() : playTrack();
 }
-function playTrack(){
+
+function playTrack() {
     curr_track.play();
     isPlaying = true;
-    track_art.classList.add('rotate');
-    wave.classList.add('loader');
     playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
-function pauseTrack(){
+
+function pauseTrack() {
     curr_track.pause();
     isPlaying = false;
-    track_art.classList.remove('rotate');
-    wave.classList.remove('loader');
     playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
 }
-function nextTrack(){
-    if(track_index < music_list.length - 1 && isRandom === false){
+
+function nextTrack() {
+    if (track_index < music_list.length - 1) {
         track_index += 1;
-    }else if(track_index < music_list.length - 1 && isRandom === true){
-        let random_index = Number.parseInt(Math.random() * music_list.length);
-        track_index = random_index;
-    }else{
+    } else {
         track_index = 0;
     }
     loadTrack(track_index);
     playTrack();
 }
-function prevTrack(){
-    if(track_index > 0){
+
+function prevTrack() {
+    if (track_index > 0) {
         track_index -= 1;
-    }else{
-        track_index = music_list.length -1;
+    } else {
+        track_index = music_list.length - 1;
     }
     loadTrack(track_index);
     playTrack();
 }
-function seekTo(){
+
+function seekTo() {
     let seekto = curr_track.duration * (seek_slider.value / 100);
     curr_track.currentTime = seekto;
 }
-function setVolume(){
+
+function setVolume() {
     curr_track.volume = volume_slider.value / 100;
 }
-function setUpdate(){
+
+function setUpdate() {
     let seekPosition = 0;
-    if(!isNaN(curr_track.duration)){
+    if (!isNaN(curr_track.duration)) {
         seekPosition = curr_track.currentTime * (100 / curr_track.duration);
         seek_slider.value = seekPosition;
 
         let currentMinutes = Math.floor(curr_track.currentTime / 60);
-        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+        let currentSeconds = Math.floor(curr_track.currentTime % 60);
         let durationMinutes = Math.floor(curr_track.duration / 60);
-        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+        let durationSeconds = Math.floor(curr_track.duration % 60);
 
-        if(currentSeconds < 10) {currentSeconds = "0" + currentSeconds; }
-        if(durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-        if(currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
-        if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
-
-        curr_time.textContent = currentMinutes + ":" + currentSeconds;
-        total_duration.textContent = durationMinutes + ":" + durationSeconds;
+        curr_time.textContent = `${currentMinutes}:${currentSeconds}`;
+        total_duration.textContent = `${durationMinutes}:${durationSeconds}`;
     }
 }
+
+loadTrack(track_index);
